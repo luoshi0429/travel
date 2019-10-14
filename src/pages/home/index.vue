65c9036a923e2a1a9c17c21b4270af3fd0990f74
liumin <liumin@polyv.net>
2019-10-14 22:53:12 +0800

    initial

---
+++ src/pages/home/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Home page1
+  </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+  background: red;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  lintOnSave: 'error',
+
+  chainWebpack: config => {
+    const oneOfsMap = config.module.rule('scss').oneOfs.store;
+    oneOfsMap.forEach(item => {
+      item.use('sass-resources-loader').loader('sass-resources-loader').options({
+        resources: './src/styles/global.scss'
+      }).end();
+    });
+  },
+
+  devServer: {
+    port: 15008,
+
+    proxy: {
+      '^/api/shop/': {
+        target: 'http://47.92.154.203:666',
+        changeOrigin: true,
+      }
+    }
+  }
+};
